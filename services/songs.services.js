1b7f40ca82efc6480ca6b677434035ebd656583a
Daniel <itzdanielmichael@gmail.com>
2021-12-12 19:13:16 -0500

    update songs.services.js

--- services/songs.services.js
+++ services/songs.services.js
@@ -3,6 +3,7 @@ import fetch from 'isomorphic-unfetch';
 export const getAllSongs = async () => {
   const response = await fetch('http://localhost:3000/api/songs/', {
     method: 'GET',
+    mode: 'no-cors',
   }).then((r) => r.json());
 
   const allSongs = response.data;
