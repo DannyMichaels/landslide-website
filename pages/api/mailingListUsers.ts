22d0c27e214122bd29f2de4966c9ca2d94964619
Daniel <itzdanielmichael@gmail.com>
2021-12-12 21:59:32 -0500

    this might work

--- pages/api/mailingListUsers.ts
+++ pages/api/mailingListUsers.ts
@@ -2,6 +2,26 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '../../lib/dbConnect';
 import MailingListUser from '../../models/MailingListUser';
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -48,3 +68,5 @@ export const config = {
     },
   },
 };
+
+module.exports = allowCors(handler);
